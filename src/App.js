bc6ea9e9bc30d20b3e50f1c1abe2c28c679b6566
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2023-04-10 17:46:43 +0200

    Update App.js
    made a design prototype
    
    Co-Authored-By: ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import { ButtonStyle, ReactButton } from './components';
-import logo from './logo.svg';
+import { ButtonStyle, ReactButton, ReactCard } from './components';
+import typeicon from './assets/types/fire.svg';
 import './style/App.css';
 import './style/reactComponents.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ReactButton buttonStyle={ButtonStyle.outlineInfo} className="something" onClick={() => console.log("hey")}>click me</ReactButton>
         <div className='splitContainer'>
           <div className='rightSplit'></div>
           <div className='leftSplit'></div>
         </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ReactCard className={"pokedex-background"}>
+          <div>
+            <ReactCard className={"leftContainer"}>
+              <ReactCard id={"pokemonPreview"}>
+                <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png' alt='pokemonImage'></img>
+              </ReactCard>
+              <h1>Bulbasaur</h1>
+              <h5>A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.</h5>
+              <div id='types'>
+                <ReactCard>
+                  <img src={typeicon} alt='svg'></img>
+                  <h4>Fire</h4>
+                </ReactCard>
+                <ReactCard>
+
+                </ReactCard>
+              </div>
+              <ReactButton buttonStyle={ButtonStyle.danger}>More info</ReactButton>
+            </ReactCard>
+            <ReactCard className={"rightContainer"}>
+              <div>
+                <ReactCard className={"pokemonListItem"}>
+                  <p>#1</p>
+                  <h4>Bulbasaur</h4>
+                </ReactCard>
+                <ReactCard className={"pokemonListItem"}>
+                  <p>#1</p>
+                  <h4>Bulbasaur</h4>
+                </ReactCard>
+                <ReactCard className={"pokemonListItem"}>
+                  <p>#1</p>
+                  <h4>Bulbasaur</h4>
+                </ReactCard>
+              </div>
+              <div>
+                <ReactButton buttonStyle={ButtonStyle.light}>Previous</ReactButton>
+                <ReactButton buttonStyle={ButtonStyle.light}>Next</ReactButton>
+              </div>
+            </ReactCard>
+          </div>
+        </ReactCard>
     </div>
   );
 }
