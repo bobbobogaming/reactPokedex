bdc543cc4e9a412f247f1fd205e7195cac238b74
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2023-04-18 14:59:42 +0200

    Update App.js
    * added method for switching between animating in or out

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import { PokePreview } from './pokePreview'
 import { PokeList } from './pokeList'
 import './style/App.css';
 import './style/reactComponents.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 // code snippet make first letter of a string uppercase:
 // text.replace(/^[a-zA-Z]/g,text.charAt(0).toUpperCase())
 
 function App() {
   let pokePreviewRef = useRef(null);
+  const [shouldAnimateIn,setAnimationDirectionToIn] = useState(true);
+  const switchAnimationDirection = () => {
+    setAnimationDirectionToIn(!shouldAnimateIn)
+  }
+  
   return (
     <div className="App">
-        <div className='splitContainer'>
+        <div className={`splitContainer animateSplit${(shouldAnimateIn)?"In":"Out"}`} style={{gap: shouldAnimateIn?0:"1500px"}}>
           <div className='rightSplit'></div>
           <div className='leftSplit'></div>
         </div>
-        <ReactCard className={"pokedex-background"}>
+        <ReactCard className={`pokedex-background animateBackground${(shouldAnimateIn)?"In":"Out"}`} style={{width: shouldAnimateIn?"40px":"1550px"}}>
           <div>
             <PokePreview ref={pokePreviewRef}/>
             <PokeList handleSection={(name)=>pokePreviewRef.current.changeSelectedItem(name)}/>
